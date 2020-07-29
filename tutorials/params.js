f630f64726fcccb8851953723a1988b2bed88d7f
unknown <yxd99324@qq.com>
2020-07-29 18:06:35 +0800

    zoom

---
+++ tutorials/params.js
@@ -0,0 +1,8 @@
+let LatLng = [39.773130218541105, 116.47677887890623];
+let LatLng1 = [39.7732, 116.4766];
+let LatLng2 = [39.76, 116.47];
+let LatLng3 = [39.78, 116.46];
+let url =
+  "http://webrd0{s}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scale=1&style=8";
+let url1 =
+  "http://wprd0{s}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=2&style=6";
