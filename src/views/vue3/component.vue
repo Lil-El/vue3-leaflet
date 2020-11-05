5b7f3bbbdc3bf8e0c8f244c79ae3a21a839b4808
unknown <yxd99324@qq.com>
2020-11-05 13:48:02 +0800

    provide-inject

---
+++ src/views/vue3/component.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    Children:
+    <span @click="handleClick">Button</span>
+  </div>
+</template>
+
+<script>
+import { inject, reactive } from "vue";
+export default {
+  setup() {
+    const state = reactive({ name: "TEST" });
+    const parent = inject("parent");
+    return {
+      handleClick() {
+        console.log(parent());
+      },
+    };
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/vue3/index.vue
+++ src/views/vue3/index.vue
@@ -2,18 +2,23 @@
   <div>
     <h3>{{ state.title }}</h3>
     <input type="text" v-model="state.name" />
+    <child />
   </div>
 </template>
 <script>
-import { watch, reactive, onMounted, toRef } from "vue";
+import child from "./component";
+import { watch, reactive, onMounted, toRef, provide } from "vue";
 export default {
   props: ["title"],
+  components: { child },
   setup(props, ctx) {
     let { attrs, emit, slots } = ctx;
     let state = reactive({
       name: "yxd",
       title: props.title,
     });
+    provide("parent", () => state.name);
+
     watch(
       () => {
         return { name: state.name, title: props.title };
@@ -27,6 +32,7 @@ export default {
       // 修改父组件的属性，子组件发生变化
       setTimeout(() => {
         emit("update:title", "Vue3 Title Change");
+        state.name = "ywd";
       }, 1000);
       // console.log("2onMounted");
     });
